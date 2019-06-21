52a06a9044e0bd8e9d7277f844f783904f7a056c
Denis Movshovich <denis.movshovich@gmail.com>
2019-06-21 22:07:01 +0300

    Первый коммит. Добавил TableView и Cell

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Denis Movshovich on 21/06/2019.
+//  Copyright © 2019 Denis Movshovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
