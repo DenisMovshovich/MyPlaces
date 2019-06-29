7b1d9d5241deea2e789fdc75615ee27afb34cd9a
Denis Movshovich <denis.movshovich@gmail.com>
2019-06-29 20:57:17 +0300

    Create placeModel to add places Add new TableView to editing places list

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let restauranNames = ["Burger King", "Шок", "Бочка", "Индокитай", "Sushi House", "Пепперони", "Sherlock's Pub", "Kitchen", "Love&Life", "Speak Easy", "Балкан Гриль"]
+    let places = Place.getPlaces()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +21,21 @@ class MainViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restauranNames.count
+        return places.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.nameLabel?.text = restauranNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restauranNames[indexPath.row])
+        cell.nameLabel?.text = places[indexPath.row].name
+        cell.locationLabel?.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
         return cell
-    }
-    
-    //MARK: - TableView Delegate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
     }
     
     /*
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,30 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Denis Movshovich on 29/06/2019.
+//  Copyright © 2019 Denis Movshovich. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restauranNames = ["Burger King", "Шок", "Бочка", "Индокитай", "Sushi House", "Пепперони", "Sherlock's Pub", "Kitchen", "Love&Life", "Speak Easy", "Балкан Гриль"]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restauranNames {
+            places.append(Place(name: place, location: "Казань", type: "Ресторан", image: place))
+        }
+        return places
+    }
+}
+
+
