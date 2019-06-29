9aa5833e0929c69432ccb903f007a502d675468b
Denis Movshovich <denis.movshovich@gmail.com>
2019-06-29 12:39:32 +0300

    Добавил места и CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Denis Movshovich on 29/06/2019.
+//  Copyright © 2019 Denis Movshovich. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
