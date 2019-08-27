4d75d48c2d2a0f01a9ccbf055dd78baaee4846ee
Denis Movshovich <denis.movshovich@gmail.com>
2019-08-28 00:32:05 +0300

    Реализовал выставление рейтинга с помощью фреймворка Cosmos. Однако на NewPlaceViewController оставил реализацию на RatingControl (кастомное решение)

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -55,29 +55,28 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         if isFiltering {
             return filteredPlaces.count
         }
-        return places.isEmpty ? 0 : places.count
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        var place = Place()
+        let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
         
-        if isFiltering {
-            place = filteredPlaces[indexPath.row]
-        } else {
-            place = places[indexPath.row]
-        }
+//        var place = Place()
+        
+//        if isFiltering {
+//            place = filteredPlaces[indexPath.row]
+//        } else {
+//            place = places[indexPath.row]
+//        }
         
         cell.nameLabel?.text = place.name
         cell.locationLabel?.text = place.location
         cell.typeLabel.text = place.type
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
-        
-        
-        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-        cell.imageOfPlace.clipsToBounds = true
-        
+        cell.cosmosView.rating = place.rating
+
         return cell
     }
     
@@ -113,12 +112,13 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
             // находим индекс выбранной ячейки
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
             // имея индекс извлекаем объект выбранной ячейки, проверяя активна ли строка поиска
-            let place: Place
-            if isFiltering {
-                place = filteredPlaces[indexPath.row]
-            } else {
-                place = places[indexPath.row]
-            }
+            let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
+//            let place: Place
+//            if isFiltering {
+//                place = filteredPlaces[indexPath.row]
+//            } else {
+//                place = places[indexPath.row]
+//            }
             
             // создаем экземпляр NewPlaceViewController
             let newPlaceVC = segue.destination as! NewPlaceViewController
