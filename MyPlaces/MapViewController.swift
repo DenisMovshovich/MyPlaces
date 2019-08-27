b5eb7bf788dd23f701f174e6d32806c1f293db43
Denis Movshovich <denis.movshovich@gmail.com>
2019-08-28 01:51:31 +0300

    Добавил MapView и кнопки для него

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Denis Movshovich on 28/08/2019.
+//  Copyright © 2019 Denis Movshovich. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
