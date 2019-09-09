01d15c6ddd93c0012e966e6227cf83e4dc628e09
Denis Movshovich <denis.movshovich@gmail.com>
2019-09-10 00:49:06 +0300

    Доработал отображение заведения на карте, добавив изображение и дефолтный pin (аннотацию)

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -12,11 +12,14 @@ import MapKit
 class MapViewController: UIViewController {
 
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
     
     @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Назначаем наш класс делегатом ответсвенным за выполнение методов протокола MKMapViewDelegate
+        mapView.delegate = self
         setupPlacemark()
 
     }
@@ -59,5 +62,38 @@ class MapViewController: UIViewController {
             
             
         }
+    }
+}
+
+// MKMapViewDelegate содержит методы для более тонкой работы с картами
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // Данный метод отвечает за отображение аннотации
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Надо убедиться, что данный объект не является аннотацией текущего метоположения пользователя
+        // Если маркером на карте является текущее местоположение пользователя, то выходим из метода
+        guard !(annotation is MKUserLocation) else { return nil }
+        // Создаем объект класса MKAnnotationView, который и представляет аннотацию на карте
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        // В том случае, если на карте не окажется ни одного представления с аннотацией, которое мы могли бы переиспользовать
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            // Отображаем аннотацию в виде баннера
+            annotationView?.canShowCallout = true
+        }
+        // Проверяем изображение (place.imageData) на nil
+        if let imageData = place.imageData {
+            // Свойство для отображения изображения.
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            // Настройка изображения
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView 
+        }
+        // Возвращаем этот объект
+        return annotationView
     }
 }
